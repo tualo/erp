fbbf540cc8fcc893744d33980842c12049d5ff99
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-12-15 22:18:28 +0100

    fixes

--- src/sql/install/buchungskonten.sql
+++ src/sql/install/buchungskonten.sql
@@ -1,7 +1,17 @@
-DELIMITER ;
+DELIMITER;
+
 CREATE TABLE IF NOT EXISTS `buchungskonten` (
-  `konto` varchar(100) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `aktiv` tinyint(4) DEFAULT 1,
-  PRIMARY KEY (`konto`)
+    `konto` varchar(100) NOT NULL,
+    `name` varchar(255) DEFAULT NULL,
+    `aktiv` tinyint(4) DEFAULT 1,
+    PRIMARY KEY (`konto`)
 );
+
+CREATE OR REPLACE VIEW `view_readtable_buchungskonten` AS
+select
+    `buchungskonten`.`konto` AS `konto`,
+    `buchungskonten`.`name` AS `name`,
+    `buchungskonten`.`aktiv` AS `aktiv`,
+    `buchungskonten`.`konto` AS `display`
+from
+    `buchungskonten`;
