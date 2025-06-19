df373a09e47ea18b2f1db9d49805030773995f3d
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-06-19 15:16:40 +0200

    add tablecheck

--- src/Checks/Tables.php
+++ src/Checks/Tables.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Tualo\Office\ERP\Checks;
 
 use Tualo\Office\Basic\Middleware\Session;
@@ -6,29 +7,34 @@ use Tualo\Office\Basic\PostCheck;
 use Tualo\Office\Basic\TualoApplication as App;
 
 
-class Tables  extends PostCheck {
-    
-    public static function test(array $config){
+class Tables  extends PostCheck
+{
+
+    public static function test(array $config)
+    {
         $clientdb = App::get('clientDB');
         if (is_null($clientdb)) return;
         $tables = [
-            'adressen'=>[],
-            'booking_circle'=>[],
-            'buchungskonten'=>[],
-            'einspeiserkennzeichen'=>[],
-            'geschaeftsstatus'=>[],
-            'kundenbetreuer'=>[],
-            'preiskategorien'=>[],
-            'rechnungsdruck'=>[],
-            'rechnungslegung'=>[],
-            'steuergruppen'=>[],
-            'vertragsarten'=>[],
-            'zahlungsarten'=>[],
-            'zahlungsschluessel'=>[]
+            'adressen' => [],
+            'booking_circle' => [],
+            'buchungskonten' => [],
+            'buchungskreise' => [],
+            'einspeiserkennzeichen' => [],
+            'geschaeftsstatus' => [],
+            'kundenbetreuer' => [],
+            'preiskategorien' => [],
+            'rechnungsdruck' => [],
+            'rechnungslegung' => [],
+            'steuergruppen' => [],
+            'vertragsarten' => [],
+            'zahlungsarten' => [],
+            'zahlungsschluessel' => []
         ];
-        self::tableCheck('ds',$tables,
-            "please run the following command: `./tm install-sql-erp --client ".$clientdb->dbname."`",
-            "please run the following command: `./tm install-sql-erp --client ".$clientdb->dbname."`"
+        self::tableCheck(
+            'ds',
+            $tables,
+            "please run the following command: `./tm install-sql-erp --client " . $clientdb->dbname . "`",
+            "please run the following command: `./tm install-sql-erp --client " . $clientdb->dbname . "`"
 
         );
     }
